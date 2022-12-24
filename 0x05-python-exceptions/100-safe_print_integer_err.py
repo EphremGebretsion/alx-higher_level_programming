6c3652eba7d8e063595c2d97461d8961ab78398a
= <=>
2022-12-24 18:12:13 +0300

    coreeection 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,10 @@ import sys
 
 
 def safe_print_integer_err(value):
+    er = "Exception: Unkown format code 'd' for object of type 'str'"
     try:
         print("{:d}".format(value))
         return (True)
     except ValueError:
-        print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
+        print(er, file=sys.stderr)
         return (False)
