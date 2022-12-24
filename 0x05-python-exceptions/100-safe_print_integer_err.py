6cd36ff4fa1e277472f7ffa9d7e6c2cad3343254
= <=>
2022-12-24 18:16:53 +0300

    correct 100-safe

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,10 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-    er = "Exception: Unkown format code 'd' for object of type 'str'"
     try:
         print("{:d}".format(value))
         return (True)
-    except ValueError:
-        print(er, file=sys.stderr)
+    except ValueError as cer:
+        print("Exception: {}".format(cer), file=sys.stderr)
         return (False)
