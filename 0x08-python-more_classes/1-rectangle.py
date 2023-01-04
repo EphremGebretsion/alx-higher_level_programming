e40968f1e7df8670b2c02e62171e33d42ee2c7e3
= <=>
2023-01-04 20:37:18 +0300

    class with geters and seters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """Rectangle class with getter and seters"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width(width)
+        self.height(height)
 
     def width(self):
         return self.__width
