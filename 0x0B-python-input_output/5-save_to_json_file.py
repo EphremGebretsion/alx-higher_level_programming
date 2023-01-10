ab6995a76a86db83a021ec056955bbbc0cd94c4b
= <=>
2023-01-11 00:32:51 +0300

    save to json file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes my_obj to a file filename using JSON"""
-    with open(filename, mode='r', encoding="utf-8") as my_file:
+    with open(filename, mode='w', encoding="utf-8") as my_file:
         json.dump(my_obj, my_file)
