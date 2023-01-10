75239fe8155f60e21fa7d30168e81bb5a968565a
= <=>
2023-01-11 00:11:54 +0300

    python I/O

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,6 +3,8 @@
 
 
 import json
+
+
 def to_json_string(my_obj):
     """ return the JSON representation of my_obj"""
     return (json.dumps(my_obj))
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,6 +3,8 @@
 
 
 import json
+
+
 def save_to_json_file(my_obj, filename):
     """writes my_obj to a file filename using JSON"""
     json.dump(my_obj, filename)
