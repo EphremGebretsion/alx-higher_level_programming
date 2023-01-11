5b6a717fd17f6734b951c4195cd4a4456a53ab18
= <=>
2023-01-11 19:21:44 +0300

    reload from json object

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" reload student from json object"""
+
+
+class Student():
+    """ student class with json dictionary convertor and reloader"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (not attrs):
+            return (self.__dict__)
+        if (type(attrs) != list):
+            return (self.__dict__)
+
+        newlist = {}
+        dic = self.__dict__
+        for i in attrs:
+            if (i in dic):
+                newlist[i] = dic[i]
+        return (newlist)
+
+    def reload_from_json(self, json):
+        dic = self.__dict__
+
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
