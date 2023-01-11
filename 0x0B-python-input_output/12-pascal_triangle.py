13b42a78b5a8cad1dc1045a41edcc28190383cf0
= <=>
2023-01-11 22:08:07 +0300

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -16,7 +16,7 @@ def pascal_next(prev_list):
 def pascal_triangle(n):
     """creates a pasical triangle list"""
     if (n < 1):
-        return None
+        return ([])
     pascal_list = []
     for i in range(1, (n + 1)):
         if (i == 1):
