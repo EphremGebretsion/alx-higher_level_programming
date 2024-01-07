1240596bf34b35d7b4a1d17c93a1434b21d3e4b9
EphremGebretsion <ephhrem@gmail.com>
2024-01-07 23:12:21 +0300

    first done

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+addition calcularing module
+checks the type of the parameters before
+calculating the result
+"""
+
+
+def add_integer(a, b=98):
+    """
+    addition function
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be integer")
+
+    return (int(a) + int(b))
