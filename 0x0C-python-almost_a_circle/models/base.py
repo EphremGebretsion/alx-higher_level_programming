434867821066a7c61afc5a3598ecf7486e718792
EphremGebretsion <ephhrem@gmail.com>
2024-01-16 02:56:12 +0300

    another fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,7 +50,11 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        dummy = cls(0, 0)
+        dummy = None
+        if (cls.__name__ == "Square"):
+            dummy = cls(1)
+        elif (cls.__name__ == "Rectangle"):
+            dummy = cls(1, 1)
         dummy.update(**dictionary)
         return dummy
 
@@ -93,7 +97,11 @@ class Base:
                 lis.append(row)
         i_list = []
         for i in lis:
-            dummy = cls(0, 0)
+            dummy = None
+            if (cls.__name__ == "Square"):
+                dummy = cls(1)
+            elif (cls.__name__ == "Rectangle"):
+                dummy = cls(1, 1)
             dummy.id = int(i[0])
             if cls.__name__ == "Square":
                 dummy.size = int(i[1])
