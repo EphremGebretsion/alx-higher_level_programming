90fc3ea93a77c07e2f38cc6474063d42573e21b0
EphremGebretsion <ephhrem@gmail.com>
2024-01-20 00:22:56 +0300

    test cont

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -2,6 +2,7 @@ import unittest
 from models.rectangle import Rectangle
 import sys
 from io import StringIO
+import json
 
 
 class TestRectangle(unittest.TestCase):
@@ -56,7 +57,7 @@ class TestRectangle(unittest.TestCase):
         my = Rectangle(3, 5, 2, 4, 1457)
         self.assertAlmostEqual(my.__str__(), '[Rectangle] (1457) 2/4 - 3/5')
 
-    def test_str(self):
+    def test_disp(self):
         my = Rectangle(2, 2)
         out = StringIO()
         sys.stdout = out
@@ -150,3 +151,28 @@ class TestRectangle(unittest.TestCase):
         d = {'x': 32, 'y': 3, 'id': 7, 'height': 3, 'width': 4}
         self.assertAlmostEqual(my.to_dictionary(), d)
 
+    def test_save(self):
+        Rectangle.save_to_file(None)
+        lis = ''
+        with open("Rectangle.json", "r") as my_file:
+            lis = my_file.read()
+        self.assertAlmostEqual(lis, '[]')
+
+        Rectangle.save_to_file([])
+        lis = ''
+        with open("Rectangle.json", "r") as my_file:
+            lis = my_file.read()
+        self.assertAlmostEqual(lis, '[]')
+
+        res = [{'x': 0, 'y': 0, 'id': 17, 'height': 2, 'width': 1}]
+        json_lis = []
+        Rectangle.save_to_file([Rectangle(1, 2, 0, 0, 17)])
+        with open("Rectangle.json", "r") as my_file:
+            json_lis = json.load(my_file)
+        self.assertAlmostEqual(json_lis, res)
+
+    def test_load(self):
+        Rectangle.save_to_file([Rectangle(12, 23, 0, 0, 17)])
+        res = {'x': 0, 'y': 0, 'id': 17, 'height': 23, 'width': 12}
+        lis = Rectangle.load_from_file()
+        self.assertAlmostEqual(lis[0].to_dictionary(), res)
