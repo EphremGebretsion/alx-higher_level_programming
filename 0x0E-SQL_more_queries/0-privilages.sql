9e54264d84301105e03387c6103327792b66c90b
EphremGebretsion <ephhrem@gmail.com>
2024-02-14 19:45:39 +0300

    list privilages

---
+++ 0x0E-SQL_more_queries/0-privilages.sql
@@ -0,0 +1,5 @@
+-- this script will display all the privilages
+-- command to display privilages for user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- command to display privilages for user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,2 @@
+more on sql
+new user, grant, privilage, join
