5455865ec3b795432dcaee1f1a31ba7c630ed427
Ephrem Gebretsion <ephhrem@gmail.com>
2024-04-24 02:08:15 +0300

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const req = require('request');
+const url = process.argv[2];
+req.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(response.statusCode);
+});
